f621c92ff29029f911c4ce4064bab875411a9171
Hoang Phuc <hoangphuc11tt@gmail.com>
2026-03-01 20:59:22 +0700

    fix: use correct SendgridApiTransport class and restore ServiceProvider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Mail;
-use Symfony\Component\Mailer\Bridge\Sendgrid\Transport\SendgridApiTransportFactory;
-use Symfony\Component\Mailer\Transport\Dsn;
+use Symfony\Component\Mailer\Bridge\Sendgrid\Transport\SendgridApiTransport;
 use Symfony\Component\HttpClient\HttpClient;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,9 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Mail::extend('sendgrid', function (array $config) {
-            return (new SendgridApiTransportFactory(HttpClient::create()))->create(
-                new Dsn('sendgrid+api', 'default', $config['key'])
-            );
+            return new SendgridApiTransport($config['key'], HttpClient::create());
         });
     }
 }
